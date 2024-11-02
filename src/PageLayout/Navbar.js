382b7b4bfe932c4c69c2564513487de9805de76b
James Dale Abeyta <abeyternater@Jamess-Air.lan>
2024-11-01 20:21:22 -0700

    Reducing navbar options

--- src/PageLayout/Navbar.js
+++ src/PageLayout/Navbar.js
@@ -18,8 +18,6 @@ export const Navbar = () => {
             <nav id="navbar1" className='navMenu' data-visible = "false">
 
                 <NavLink to="/" className="navbarbtn"><h1>JDA</h1></NavLink>
-                <NavLink to="/About" className="navbarbtn"><h1>About</h1></NavLink>
-                <NavLink to="/Blog" className="navbarbtn"><h1>Blog</h1></NavLink>
                 <NavLink to="/Projects" className="navbarbtn"><h1>Projects</h1></NavLink>
                 <NavLink to="/Contact" className="navbarbtn"><h1>Contact</h1></NavLink>
         
